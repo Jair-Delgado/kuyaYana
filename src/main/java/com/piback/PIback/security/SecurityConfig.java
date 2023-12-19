9fe736ee9c9ed200c09c9deeb7ec7055f60e8ae7
T1Erick <guevaraerick49@gmail.com>
2023-12-18 23:45:35 -0500

    Cambios fornt y back

--- src/main/java/com/piback/PIback/security/SecurityConfig.java
+++ src/main/java/com/piback/PIback/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.piback.PIback.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,6 +13,9 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.piback.PIback.JWT.JWTAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -30,9 +34,10 @@ public class SecurityConfig {
         .csrf(csrf -> 
         csrf
         .disable())
+        .cors(Customizer.withDefaults())
         .authorizeHttpRequests(authRequest ->
          authRequest
-         .requestMatchers("/auth/**").permitAll()
+         .requestMatchers("/auth/**","http:localhost/4200/*").permitAll()
          .anyRequest().authenticated()
         )
         .sessionManagement(sessionManager->
